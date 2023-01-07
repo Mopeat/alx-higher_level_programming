7372c6a0c295fb16d0631f06f8704bd847df33ee
Atoyebi Mudasiry <atyemi77@hotmail.com>
2023-01-07 18:03:31 +0000

    program that prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for i in range(00, 100):
     if i != 99:
         print("{:02d}, ".format(i), end='')
     else:
-        print("{:02d} ".format(i), end='\n')
+        print("{:02d}".format(i), end='\n')
