e1b6046ad5d5c9bf44a18a24a17f58fa370bcffe
Atoyebi Mudasiry <atyemi77@hotmail.com>
2023-01-09 02:57:55 +0000

    function that prints the last digit of a number.

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,5 +4,5 @@ def print_last_digit(number):
         last_digit = number % 10
     else:
         last_digit = -number % 10
-    print("{:d}".format(last_digit), end = '')
+    print("{:d}".format(last_digit), end='')
     return last_digit
