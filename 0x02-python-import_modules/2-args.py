530718da6717a1767aebf615aa00eaa43cf30194
Atoyebi Mudasiry <atyemi77@hotmail.com>
2023-01-19 17:53:17 +0000

    Done

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv)
+    if n == 1:
+        print("{} argument:".format(n - 1))
+    elif n == 2:
+        print("{} argument:".format(n - 1))
+    else:
+        print("{} arguments:".format(n - 1))
+
+    if n > 1:
+        for i in range(1, n):
+            print("{} : {}".format(i, sys.argv[i]))
+        i += 1 
